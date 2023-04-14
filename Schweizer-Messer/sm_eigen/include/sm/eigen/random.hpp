51ec5b97a44b48a301f73d3506f288af86668e58
lacie-life <00sao00ios00@gmail.com>
2023-04-14 21:47:20 +0700

    []

---
+++ Schweizer-Messer/sm_eigen/include/sm/eigen/random.hpp
@@ -0,0 +1,14 @@
+#ifndef SM_EIGEN_RANDOM_HPP
+#define SM_EIGEN_RANDOM_HPP
+#include <eigen3/Eigen/Core>
+
+namespace sm {
+namespace eigen {
+
+Eigen::VectorXd randn(unsigned dim);
+
+} // namespace eigen
+} // namespace sm
+
+
+#endif /* SM_EIGEN_RANDOM_HPP */
